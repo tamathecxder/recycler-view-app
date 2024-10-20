789d58e27ed62a619539dd24640d5e6218548e4e
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-10-20 20:56:27 +0700

    setup item click callback

--- app/src/main/java/com/tama/recyclerviewapp/ListHeroAdapter.kt
+++ app/src/main/java/com/tama/recyclerviewapp/ListHeroAdapter.kt
@@ -8,6 +8,16 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Hero)
+    }
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
         return ListViewHolder(view)
@@ -18,6 +28,7 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adap
         holder.imgPhoto.setImageResource(photo)
         holder.tvName.text = name
         holder.tvDescription.text = description
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listHero[holder.adapterPosition]) }
     }
 
     override fun getItemCount(): Int = listHero.size
