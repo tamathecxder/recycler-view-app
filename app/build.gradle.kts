bd5d19bdef5b142ae424fa9b8041b82a08bcdd01
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-10-26 13:21:57 +0700

    replace new dependencies declaration with version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,5 +47,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation(libs.circleimageview)
+    implementation(libs.glide)
 }
